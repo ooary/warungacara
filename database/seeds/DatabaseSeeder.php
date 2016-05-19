9ab79dd589588ccfb426cf79553b496d1bf46eae
wedus98 <aryinteristi@gmail.com>
2016-05-20 06:44:50 +0700

    Tambah Seeder Function

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $this ->call(CategotySeeder::class);
+        $this ->call(ProdySeeder::class);
     }
 }
